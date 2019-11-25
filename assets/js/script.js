00c208f91a101e3895c948f07cc4b83c6684bc93
Arisa <bakaichi.arian@gmail.com>
2019-11-25 18:12:34 +0900

    responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,12 @@ var imageSlide = document.getElementsByClassName("works")[0].getElementsByTagNam
 var nowIndex = 0;
 imageSlide[nowIndex].classList.add("show");
 var slideTimer;
-TweenMax.fromTo(".bar" , 5 , {width: "0%"}, {width: "100%", ease : "Linear"});
+TweenMax.fromTo(".bar", 5, { width: "0%" }, { width: "100%", ease: "Linear" });
 var getColor = imageSlide[nowIndex].getAttribute("id");
 document.body.style.backgroundColor = getColor;
 
 function sliderSlide(val) {
-	TweenMax.fromTo(".bar" , 5 , {width: "0%", ease : "Linear"}, {width: "100%", ease : "Linear"} );
+	TweenMax.fromTo(".bar", 5, { width: "0%", ease: "Linear" }, { width: "100%", ease: "Linear" });
 	isChanging = true;
 	imageSlide[nowIndex].classList.remove("show");
 	nowIndex = val;
@@ -19,49 +19,68 @@ function sliderSlide(val) {
 }
 
 //Home transition | auto
-setInterval(function(){
+setInterval(function () {
 	var index = nowIndex + 1;
-	if(index > length){
+	if (index > length) {
 		index = 0;
 	}
 	sliderSlide(index);
-},5000);
+}, 5000);
 
 //Home transition | clicking arrow
-document.querySelector(".column_left").addEventListener("click", function(){
+document.querySelector(".column_left").addEventListener("click", function () {
 	var index = nowIndex - 1;
-	if(index < 0){
-	  index = length;
+	if (index < 0) {
+		index = length;
 	}
 	sliderSlide(index);
 }, false);
-document.querySelector(".column_right").addEventListener("click", function(){
+document.querySelector(".column_right").addEventListener("click", function () {
 	var index = nowIndex + 1;
-	if(index > length){
-	  index = 0;
+	if (index > length) {
+		index = 0;
 	}
 	sliderSlide(index);
 }, false);
 
 
 //Modal
-var about = document.querySelectorAll(".icon-about");
-var close = document.querySelectorAll(".icon-close");
+var abouts = document.querySelectorAll(".icon-about");
+var closes = document.querySelectorAll(".icon-close");
+var modals = document.querySelectorAll(".modal");
 
-for(var i = 0; i<about.length; i++){
-	about[i].addEventListener("click", function(){
-		document.querySelector(".modal").style.display = 'block';
+abouts.forEach(function (about) {
+	about.addEventListener("click", function () {
+		modals.forEach(function (modal) {
+			modal.classList.add("show");
+		});
 	});
-}
-for(var i = 0; i < close.length; i++){
-	close[i].addEventListener("click", function(){
-		document.querySelector(".modal").style.display = 'none';
+});
+closes.forEach(function (close) {
+	close.addEventListener("click", function () {
+		modals.forEach(function (modal) {
+			modal.classList.remove("show");
+		});
 	});
-}
+});
 
 
 //Name animation
-var name = document.querySelector(".name")
+var nameDivs = document.querySelectorAll(".name");
+nameDivs.forEach(function (nameDiv) {
 
-console.log(about);
-console.log(document.querySelector("object"));
+	var xhr = new XMLHttpRequest();
+	xhr.addEventListener("load", function () {
+		nameDiv.innerHTML = xhr.responseText;
+	});
+	xhr.open("GET", nameDiv.getAttribute("data-src"));
+	xhr.send();
+});
+
+
+//three js
+window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+	// ここに処理を追加していきます
+}
